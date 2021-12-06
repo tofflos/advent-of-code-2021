e9c2530b165ad8a9d49fc3c94db65f7a76bc5bb0
tofflos <erik.ostlund@gmail.com>
2021-12-06 18:29:22 +0100

    Minor tweaks

--- day06/src/main/java/com/adventofcode2021/Main.java
+++ day06/src/main/java/com/adventofcode2021/Main.java
@@ -10,33 +10,29 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-        var fish = Arrays.stream(Files.readString(Paths.get("6.in")).split(","))
-                .map(Integer::parseInt)
-                .toList();
-
         var frequencies = new long[9];
 
-        fish.stream()
+        Arrays.stream(Files.readString(Paths.get("6.in")).split(","))
+                .map(Integer::parseInt)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet().forEach(entry -> frequencies[entry.getKey()] = entry.getValue());
+                .forEach((k, v) -> frequencies[k] = v);
 
         System.out.println("Part 1: " + calculate(frequencies, 80));
         System.out.println("Part 2: " + calculate(frequencies, 256));
     }
     
     static long calculate(long [] frequencies, int rounds) {
-        var current = Arrays.copyOf(frequencies, frequencies.length);
+        var copy = Arrays.copyOf(frequencies, frequencies.length);
 
         for (int i = 0; i < rounds; i++) {
-            var zeroes = current[0];
+            var zeroes = copy[0];
 
-            System.arraycopy(current, 1, current, 0, 8);
-            current[6] += zeroes;
-            current[8] = zeroes;
+            System.arraycopy(copy, 1, copy, 0, 8);
+            copy[6] += zeroes;
+            copy[8]  = zeroes;
 
         }
         
-        return Arrays.stream(current).sum();
+        return Arrays.stream(copy).sum();
     }
 }

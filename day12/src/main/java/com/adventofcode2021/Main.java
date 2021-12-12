112518f81890882d8777b532b577bd609fe3a225
tofflos <erik.ostlund@gmail.com>
2021-12-12 15:59:43 +0100

    Minor tweaks

--- day12/src/main/java/com/adventofcode2021/Main.java
+++ day12/src/main/java/com/adventofcode2021/Main.java
@@ -5,10 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.function.BiPredicate;
 import java.util.function.Function;
@@ -26,43 +24,44 @@ public class Main {
                 })
                 .collect(Collectors.groupingBy(t -> t[0], Collectors.mapping(t -> t[1], Collectors.toSet())));
 
-        System.out.println("Part 1: " + solve(caves, (path, next) -> !path.contains(next) || isBig(next)));
-        System.out.println("Part 2: " + solve(caves, (path, next) -> {
-            var frequencies = path.stream().filter(Main::isSmall).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println("Part 1: " + solve(caves, (visited, next) -> !visited.contains(next) || isBig(next)));
+        System.out.println("Part 2: " + solve(caves, (visited, next) -> {
+            var frequencies = visited.stream().filter(Main::isSmall).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+            
 
-            return !"start".equals(next) && (isBig(next) || !path.contains(next) || frequencies.values().stream().noneMatch(frequency -> frequency > 1));
+            return !"start".equals(next) && (isBig(next) || !visited.contains(next) || frequencies.values().stream().noneMatch(frequency -> frequency > 1));
         }));
     }
 
-    static boolean isBig(String name) {
-        return Character.isUpperCase(name.charAt(0));
+    static boolean isBig(String cave) {
+        return Character.isUpperCase(cave.charAt(0));
     }
 
-    static boolean isSmall(String name) {
-        return Character.isLowerCase(name.charAt(0)) && !"start".equals(name) && !"end".equals(name);
+    static boolean isSmall(String cave) {
+        return Character.isLowerCase(cave.charAt(0)) && !"start".equals(cave) && !"end".equals(cave);
     }
     
-    static int solve(Map<String, Set<String>> caves, BiPredicate<Deque<String>, String> predicate) {
-        var branches = new ArrayDeque<Deque<String>>();
+    static int solve(Map<String, Set<String>> caves, BiPredicate<Deque<String>, String> isVisitable) {
+        var candidates = new ArrayDeque<Deque<String>>();
         var paths = new ArrayDeque<Deque<String>>();
 
-        branches.push(new ArrayDeque<>(List.of("start")));
+        candidates.push(new ArrayDeque<>(List.of("start")));
         
-        while (!branches.isEmpty()) {
-            var branch = branches.pop();
-            var current = branch.peek();
+        while (!candidates.isEmpty()) {
+            var visited = candidates.pop();
+            var cave = visited.peek();
 
-            if ("end".equals(current)) {
-                paths.add(branch);
+            if ("end".equals(cave)) {
+                paths.add(visited);
                 continue;
             }
 
-            for (var next : caves.get(current)) {
-                if (predicate.test(branch, next)) {
-                    var b = new ArrayDeque<String>(branch);
-                    b.push(next);
+            for (var neighbour : caves.get(cave)) {
+                if (isVisitable.test(visited, neighbour)) {
+                    var candidate = new ArrayDeque<String>(visited);
+                    candidate.push(neighbour);
 
-                    branches.push(b);
+                    candidates.push(candidate);
                 }
             }
         }
